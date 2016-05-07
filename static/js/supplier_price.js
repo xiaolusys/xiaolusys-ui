ef151ccb3900b6fa8754ed42ea5b16b680b9ac60
wej <enjun.wang@xiaolu.so>
2016-05-07 12:14:30 +0800

    saleproduct

--- static/js/supplier_price.js
+++ static/js/supplier_price.js
@@ -1,19 +1,27 @@
 function calcSalePrice(cost){
     var salePrice = NaN;
     if(!isNaN(cost)){
-        var onSalePrice = Math.round(parseFloat(cost) / .65 + 8);
-        var onSalePriceStr = String(onSalePrice);
-        var lastDigit = parseInt(onSalePriceStr[onSalePriceStr.length - 1]);
-        if(lastDigit < 5)
-            salePrice = Math.max(Math.floor(onSalePrice / 10) - 1, 0) * 10 + 9.9;
-        else
-            salePrice = Math.floor(onSalePrice / 10) * 10 + 9.9;
-        // (9.9, 39.9]区间内的销售价统一改成39.9
-        if(salePrice > 9.9  && salePrice <= 39.9)
-            salePrice = 39.9;
-        // (99.9, 109.9]区间内的销售价统一改成109.9
-        if(salePrice > 99.9 && salePrice <= 109.9)
-            salePrice = 99.9;
+        cost = parseFloat(cost);
+        if(cost <= 15){
+            if(cost <= 10)
+                salePrice = 19.9;
+            else
+                salePrice = 29.9;
+        }
+        else{
+            var onSalePrice = Math.round(cost / .65 + 8);
+            var onSalePriceStr = String(onSalePrice);
+            var lastDigit = parseInt(onSalePriceStr[onSalePriceStr.length - 1]);
+            if(lastDigit < 5)
+                salePrice = Math.max(Math.floor(onSalePrice / 10) - 1, 0) * 10 + 9.9;
+            else
+                salePrice = Math.floor(onSalePrice / 10) * 10 + 9.9;
+
+            if(salePrice > 9.9  && salePrice <= 39.9)
+                salePrice = 39.9;
+            else if(salePrice > 99.9 && salePrice <= 109.9)
+                salePrice = 99.9;
+        }
     }
     return salePrice;
 }
