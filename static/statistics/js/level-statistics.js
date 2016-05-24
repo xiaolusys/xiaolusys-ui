a1a92e65aa3e1efa01fe2c768a1db350d7859ca2
ahlinjie <1051631394@qq.com>
2016-05-24 17:50:02 +0800

    add a admin js file for statistics admin page

---
+++ static/statistics/js/level-statistics.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jishu_linjie on 5/24/16.
+ */
+function displayPic(url) {
+    layer.open({type: 2, title: '图片', shadeClose: true, shade: 0.8, area: ['620px', '90%'], content: url});
+}
