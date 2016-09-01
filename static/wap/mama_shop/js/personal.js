d345844ff53deb7f622c9a9f77156bb7b98a6026
Zifei Zhong <zfz@xiaolumeimei.com>
2016-09-01 15:46:13 +0800

    add loadTrialMama

--- static/wap/mama_shop/js/personal.js
+++ static/wap/mama_shop/js/personal.js
@@ -2,6 +2,45 @@
 //var BASE_URL = 'http://staging.xiaolumeimei.com';
 var BASE_URL = 'http://m.xiaolumeimei.com';
 
+var trial_mama_next_page = null;
+
+function renderTrialMamaList(res) {
+    if (res) {
+        var invite_num = res['count'];
+        trial_mama_next_page = res['next'];
+        var arr = res['results'];
+        for (var i=0; i<arr.length; ++i) {
+            content.push('<div class="attender-row">');
+            content.push('<div class="attender-left">');
+            content.push('<img src="'+arr[i].thumbnail+'" class="img-circle" style="height:100%;">');
+            content.push('</div><div class="attender-middle">');
+            content.push('<p class="attender-name">'+arr[i].nick+'</p>');
+            content.push('<p>'+get_time(arr[i].charge_time)+'</p>');
+            content.push('</div><div class="attender-right">');
+            if (i == 0) {
+                content.push('<p class="signup-status">奖励20元</p>');
+            } else {
+                content.push('<p class="signup-status">奖励12元</p>');
+            }
+            content.push('<p>'+get_date(arr[i].charge_time)+'</p>');
+            content.push('</div></div>');
+            $("#id-invite-list").append(content.join(''));
+        }
+    }
+}
+
+function loadTrialMamaNextPage() {
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var windowHeight = $(window).height();
+    var documentHeight = $(document).height();
+
+    if (scrollTop >= documentHeight - windowHeight - 20) {
+        if (nextPage) {
+            $.ajax({url:trial_mama_next_page, success:renderTrialMamaList});
+        }
+    }
+}
+
 function loadMamaFortune() {
     var fortune_url = '/rest/v2/mama/fortune';
     var url = BASE_URL + fortune_url;
@@ -29,8 +68,20 @@ function loadMamaFortune() {
     $.ajax({url:url, success:callback});
 }
 
-function loadInviteMama(){
-    
+function get_time(charge_time) {
+    var idx = charge_time.search('T');
+    return charge_time.substr(idx+1);
+}
+
+function get_date(charge_time) {
+    var idx = charge_time.search('T');
+    return charge_time.substr(0,idx);
+}
+
+function loadTrailMama(){
+    var fortune_url = '/rest/v2/pmt/xlmm/get_referal_mama?last_renew_type=trial';
+    var url = BASE_URL + fortune_url;
+    $.ajax({url:url, success:renderTrialMamaList});
 }
 
 
@@ -52,10 +103,12 @@ function getParamValue(key) {
 }
 
 $(document).ready(function() {
-    //var openid = getParamValue("openid");
-    //var appkey = getParamValue("appkey");
-    
-    //loadMamaFortune(openid, appkey);
     loadMamaFortune();
+    loaTrialMama();
+
+    $(window).on("scroll", function () {
+        loadTrialMamaNextPage();
+    });
+
 });
 
