fb006b396ea5a2ba226c22c15cfe0586e6270ac0
wulei <crackwl@163.com>
2016-11-14 15:43:41 +0800

    return to http

--- static/wap/mama_shop/js/mama_cashout.js
+++ static/wap/mama_shop/js/mama_cashout.js
@@ -1,5 +1,5 @@
 //var BASE_URL = 'http://127.0.0.1:8000';
-//var BASE_URL = 'http://staging.xiaolumeimei.com';
+//var BASE_URL =  'http://staging.xiaolumeimei.com';
 var BASE_URL = 'http://m.xiaolumeimei.com';
 
 var trial_mama_next_page = null;
